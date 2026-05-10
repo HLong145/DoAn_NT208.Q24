505872074a4bf33833401fb7682f85344fdc55fe
yuwkaa <nguyenlethanhnam3010@gmail.com>
2026-05-10 18:27:58 +0700

    đăng ký Like entity vào TweetsModule để query like status

--- Evolix_backend/src/tweets/tweets.module.ts
+++ Evolix_backend/src/tweets/tweets.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { TweetsService } from './tweets.service';
 import { TweetsController } from './tweets.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Tweet } from './entities/tweet.entity'; 
+import { Tweet } from './entities/tweet.entity';
 import { Comment } from '../comments/entities/comment.entity';
+import { Like } from '../likes/entities/like.entity';
 import { FollowsModule } from '../follows/follows.module';
 import { RealtimeModule } from '../realtime/realtime.module';
 import { UsersModule } from '../users/users.module';
@@ -12,7 +13,7 @@ import { BookmarksModule } from '../bookmarks/bookmarks.module';
 
 @Module({
   // Register the Tweet entity to use TypeORM repository within this module
-  imports: [TypeOrmModule.forFeature([Tweet, Comment]), FollowsModule, RealtimeModule, UsersModule, NotificationsModule, BookmarksModule], 
+  imports: [TypeOrmModule.forFeature([Tweet, Comment, Like]), FollowsModule, RealtimeModule, UsersModule, NotificationsModule, BookmarksModule],
   controllers: [TweetsController],
   providers: [TweetsService],
 })
